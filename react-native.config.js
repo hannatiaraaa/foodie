4410ed234501bb8dcf6c89ce0bfe40b519a469ae
hannatiaraaa <hannatiara@gmail.com>
2023-08-28 07:25:19 +0700

    Import new modules and link vector icons module

--- react-native.config.js
+++ react-native.config.js
@@ -5,10 +5,10 @@ module.exports = {
   },
   assets: ['./src/assets/fonts/'],
   dependencies: {
-    //   'react-native-vector-icons': {
-    //     platforms: {
-    //       ios: null,
-    //     },
-    //   },
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
   },
 };
