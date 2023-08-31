7a6a18d88706ee82b5744344f4e1075c7e8f8f06
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-08-31 15:30:10 +0700

    Define food labels

---
+++ src/configs/foodLabels.ts
@@ -0,0 +1,32 @@
+export const foodLabels = [
+  {
+    id: 'veryHealthy',
+    label: 'healthy',
+    color: '#E0004d',
+    icon: 'hand-holding-medical',
+  },
+  {
+    id: 'glutenFree',
+    label: 'gluten free',
+    color: '#C87036',
+    icon: 'plate-wheat',
+  },
+  {
+    id: 'dairyFree',
+    label: 'dairy free',
+    color: '#368DCB',
+    icon: 'cow',
+  },
+  {
+    id: 'vegetarian',
+    label: 'vegetarian',
+    color: '#FBB03B',
+    icon: 'egg',
+  },
+  {
+    id: 'vegan',
+    label: 'vegan',
+    color: '#15C458',
+    icon: 'leaf',
+  },
+];
